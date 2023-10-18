67eabd20d598315df7c9596bddd48052a5ddc592
VanderleiJunior <vanderleioleveira76@gmail.com>
2023-10-18 16:57:35 -0300

    post new user

--- src/app/api/v1/user/user-controller.js
+++ src/app/api/v1/user/user-controller.js
@@ -1,10 +1,24 @@
 import userBusiness from "./user-business.js";
 
 const userController = {
-  getUsers: async (req, res) => {
-    const user = req.body.user;
-    const result = await userBusiness.find(user);
+  get: async (req, res) => {
+    const userId = req.params.userId;
+    const result = await userBusiness.find(userId);
     return res.json(result).status(200);
+  },
+  post: async (req, res) => {
+    const user = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    if (!user.name | !user.email | !user.password) {
+      return res.status(400).send("Email, name and password is required");
+    }
+
+    const result = await userBusiness.create(user);
+    return res.status(result.status || 201).send(result.data);
   },
 };
 
