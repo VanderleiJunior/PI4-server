9ae07506732ff4a550d1c5cbf0949e8a52676875
VanderleiJunior <vanderleioleveira76@gmail.com>
2023-11-04 14:18:11 -0300

    remove cors

--- src/app/config/middleware/index.js
+++ src/app/config/middleware/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors";
+//import cors from "cors";
 import swaggerJSON from "./swagger.json" assert { type: "json" };
 import swaggerUi from "swagger-ui-express";
 
@@ -8,7 +8,7 @@ import router from "./router.js";
 const swaggerDocs = swaggerUi.setup(swaggerJSON);
 
 const appMiddleware = (app) => {
-  app.use(cors());
+  //app.use(cors());
   app.use(express.json());
   app.use(router);
   // app.use("/api/v1/api-docs", swaggerUi.serve, swaggerDocs);
