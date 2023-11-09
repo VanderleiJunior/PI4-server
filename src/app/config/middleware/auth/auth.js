c37d608be7fae2317d0361a7ec78c6b046fa2a44
VanderleiJunior <101458265+VanderleiJunior@users.noreply.github.com>
2023-11-08 21:51:36 -0300

    Update auth.js

--- src/app/config/middleware/auth/auth.js
+++ src/app/config/middleware/auth/auth.js
@@ -4,20 +4,24 @@ const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
+    console.error("No token provided");
     return res.status(401).send({ error: "No token provided" });
   }
   const parts = authHeader.split(" ");
 
   if (!parts.length === 2) {
+    console.error("Token error");
     return res.status(401).send({ error: "Token error" });
   }
   const [scheme, codToken] = parts;
   if (!/^Bearer$/i.test(scheme)) {
+    console.error("Token no formatted");
     return res.status(401).send({ error: "Token no formatted" });
   }
 
   const id = await token.validToken(codToken);
   if (!id) {
+    console.error("Token invalid or expired");
     return res.status(401).send({ error: "Token invalid or expired" });
   }
   req.userId = id;
