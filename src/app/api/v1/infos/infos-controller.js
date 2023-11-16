dc84c00015dae28bb45d2b7fa3c2e22bfc794d59
Vanderlei Junior <vanderleioleveira76@gmail.com>
2023-11-16 11:33:16 -0300

    removed comments

--- src/app/api/v1/infos/infos-controller.js
+++ src/app/api/v1/infos/infos-controller.js
@@ -38,17 +38,7 @@ const infosController = {
     } else {
       return res.status(500).send(result.data);
     }
-  },
-  // populate: async (req, res) => {
-  //   const month = req.params.month;
-  //   const result = await statisticsBusiness.populate(month);
-
-  //   if (result.status) {
-  //     return res.send(result.data).status(result.status);
-  //   }
-
-  //   return res.json(data).status(201);
-  // },
+  }
 };
 
 export default infosController;
