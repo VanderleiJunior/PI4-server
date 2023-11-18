ab24bafa158849dc8dc9f27a20cce394a9b4e0f3
Vanderlei Junior <vanderleioleveira76@gmail.com>
2023-11-18 14:18:38 -0300

    fix mqtt

--- src/app/config/broker/consumer.js
+++ src/app/config/broker/consumer.js
@@ -1,56 +1,59 @@
-import mqtt from 'mqtt';
-import axios from 'axios';
+import mqtt from "mqtt";
+import axios from "axios";
 
 // HiveMQ Cloud Cluster settings
 const MQTT_BROKER = "eb74db825f97475fb842783f5553a247.s2.eu.hivemq.cloud";
-const MQTT_PORT = 8883;  // Port for secure MQTT (TLS/SSL)
-const MQTT_TOPIC = "your/topic";  // Atualize com o tópico desejado
-const API_ENDPOINT = "https://pi-4-server.vercel.app/api/v1/infos";  // Atualize com o endpoint da sua API
+const MQTT_PORT = 8883; // Port for secure MQTT (TLS/SSL)
+const MQTT_TOPIC = "your/topic"; // Atualize com o tópico desejado
+const API_ENDPOINT = "https://pi-4-server.vercel.app/api/v1/infos"; // Atualize com o endpoint da sua API
 
 const consumer = () => {
-    const client = mqtt.connect({
+  const client = mqtt.connect({
     host: MQTT_BROKER,
     port: MQTT_PORT,
     username: "cassiotakarada",
     password: "Kunglao7*",
-    protocol: 'mqtts',  // TLS/SSL
-    rejectUnauthorized: true,  // Certificado SSL é exigido
-    });
+    protocol: "mqtts", // TLS/SSL
+    rejectUnauthorized: true, // Certificado SSL é exigido
+  });
 
-    client.on('connect', () => {
+  client.on("connect", () => {
     console.log(`Connected to MQTT broker`);
     client.subscribe(MQTT_TOPIC);
-    });
+  });
 
-    client.on('message', (topic, message) => {
+  client.on("message", (topic, message) => {
     console.log(`Received message: ${message.toString()} on topic ${topic}`);
     try {
-        // Parse the received message payload
-        const msgPayload = JSON.parse(message.toString());
+      // Parse the received message payload
+      const msgPayload = JSON.parse(message.toString());
 
-        // Prepare data in the format expected by your API
-        const data = {
-        equipmentSerialNumber: msgPayload.equipmentSerialNumber,    
+      // Prepare data in the format expected by your API
+      const data = {
+        equipmentSerialNumber: msgPayload.equipmentSerialNumber,
         temperature: msgPayload.airTemperature,
-        soilMoisture: msgPayload.soilHumidity,
-        airMoisture: msgPayload.airHumidity,
-        };
+        soilMoisture: msgPayload.soilMoisture,
+        airMoisture: msgPayload.airMoisture,
+      };
 
-        // Send data to your API endpoint
-        axios.post(API_ENDPOINT, data)
-        .then(response => {
-            console.log(`Data sent to API, response status code ${response.status}`);
+      // Send data to your API endpoint
+      axios
+        .post(API_ENDPOINT, data)
+        .then((response) => {
+          console.log(
+            `Data sent to API, response status code ${response.status}`
+          );
         })
-        .catch(error => {
-            console.error(`Error: ${error.message}`);
-            if (error.response) {
+        .catch((error) => {
+          console.error(`Error: ${error.message}`);
+          if (error.response) {
             console.error("Response data:", error.response.data);
-            }
+          }
         });
     } catch (e) {
-        console.error(`Error: ${e.message}`);
+      console.error(`Error: ${e.message}`);
     }
-    });
+  });
 };
 
 export default consumer;
