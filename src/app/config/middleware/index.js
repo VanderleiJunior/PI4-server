604826e9c579249b83f73e94b03c0c5782b926e4
PedroNevesHespanhol <pedroneveshespanhol@gmail.com>
2023-11-18 11:37:54 -0300

    alterando swagger

--- src/app/config/middleware/index.js
+++ src/app/config/middleware/index.js
@@ -1,16 +1,16 @@
 import express from "express";
 import cors from "cors";
-//import swaggerJSON from "./swagger.json" assert { type: "json" };
-//import swaggerUi from "swagger-ui-express";
+// import swaggerJSON from "./swagger.json" assert { type: "json" };
+// import swaggerUi from "swagger-ui-express";
 
 import router from "./router.js";
 
-//const swaggerDocs = swaggerUi.setup(swaggerJSON);
+// const swaggerDocs = swaggerUi.setup(swaggerJSON);
 
 const appMiddleware = (app) => {
   app.use(cors());
   app.use(express.json());
-  //app.use("/api/v1/api-docs", swaggerUi.serve, swaggerDocs);
+  // app.use("/api/v1/api-docs", swaggerUi.serve, swaggerDocs);
   app.use(router);
 };
 
